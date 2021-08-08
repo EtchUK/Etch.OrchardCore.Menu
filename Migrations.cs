a58748b9bf624c24774c6aec82283eb8e2ba3250
Peter Keating <pete.keating@etchuk.com>
2021-08-08 11:05:16 +0100

    Add ImageMenuItem
    Added new menu item that allows editors to choose an image from the
    media library that will be displayed as a link on the front-end. Content
    editors can either choose to link to a content item or provide an
    external URL.

---
+++ Migrations.cs
@@ -0,0 +1,22 @@
+﻿using OrchardCore.Data.Migration;
+using OrchardCore.Recipes.Services;
+using System.Threading.Tasks;
+
+namespace Etch.OrchardCore.Menu
+{
+    public class Migrations : DataMigration
+    {
+        private readonly IRecipeMigrator _recipeMigrator;
+
+        public Migrations(IRecipeMigrator recipeMigrator)
+        {
+            _recipeMigrator = recipeMigrator;
+        }
+
+        public async Task<int> CreateAsync()
+        {
+            await _recipeMigrator.ExecuteAsync("create.recipe.json", this);
+            return 1;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Data.Migration;
+using OrchardCore.Modules;
+
+namespace Etch.OrchardCore.Menu
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IDataMigration, Migrations>();
+        }
+    }
+}
