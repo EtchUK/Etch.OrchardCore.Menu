7556070ac1d06609bea42116121a3b805211fbf0
Peter Keating <pete.keating@etchuk.com>
2021-09-04 10:32:39 +0100

    Upgrade to 1.0.0

--- Manifest.cs
+++ Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.Modules.Manifest;
 [assembly: Module(
     Author = "Etch UK Ltd.",
     Category = "Navigation",
-    Description = "Provides custom menu items that aren't available by default.",
+    Description = "Provides additional menu items content types.",
     Name = "Custom Menu Items",
     Version = "0.2.0",
     Website = "https://etchuk.com",
