e589fd58bb52b66acedd8621f62f1ccf215b9dc9
Peter Keating <pete.keating@etchuk.com>
2021-10-31 16:20:15 +0000

    Upgrade OrchardCore to 1.1.0

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Navigation",
     Description = "Provides additional menu items content types.",
     Name = "Custom Menu Items",
-    Version = "0.2.0",
+    Version = "1.1.0",
     Website = "https://etchuk.com",
     Dependencies = new[] { "Etch.OrchardCore.Fields.Code", "OrchardCore.Media" }
 )]
