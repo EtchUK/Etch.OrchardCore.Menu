f19a68d79e31635a50f6237a6f192a62120376c7
Peter Keating <pete.keating@etchuk.com>
2021-11-18 19:25:21 +0000

    Attach `LinkVisualPart` on `ContentMenuItem` & `LinkMenuItem`
    Attached new link visual part in order for content editors to define
    visual appearance of a menu item. Moved various menu templates from
    theme to this module, which required a dependency on `OrchardCore.Menu`
    to override templates from another module.
    
    https://github.com/EtchUK/Etch.OrchardCore.Widgets/pull/88

--- Manifest.cs
+++ Manifest.cs
@@ -7,5 +7,5 @@ using OrchardCore.Modules.Manifest;
     Name = "Custom Menu Items",
     Version = "1.1.0",
     Website = "https://etchuk.com",
-    Dependencies = new[] { "Etch.OrchardCore.Fields.Code", "OrchardCore.Media" }
+    Dependencies = new[] { "Etch.OrchardCore.Fields.Code", "OrchardCore.Media", "OrchardCore.Menu", "Etch.OrchardCore.Widgets" }
 )]
